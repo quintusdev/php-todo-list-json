6b3dd50fedd847f35aadd7d57f9e90590f4c95f8
Quintusdev <quintusdev92@gmail.com>
2023-07-06 16:25:48 +0200

    [BONUS] Da terminare parte per eliminare le task

--- server.php
+++ server.php
@@ -1,34 +1,36 @@
 <?php
-// Percorso del file JSON
-$jsonFilePath = 'data/todoList.json';
+    // Percorso del file JSON
+    $jsonFilePath = 'data/todoList.json';
 
-// Recupera il contenuto del file JSON
-$jsonContent = file_get_contents($jsonFilePath);
+    // Recupera il contenuto del file JSON
+    $jsonContent = file_get_contents($jsonFilePath);
 
-// Converte il contenuto in un array associativo
-$todoList = json_decode($jsonContent, true);
+    // Converte il contenuto in un array associativo
+    $todoList = json_decode($jsonContent, true);
 
-// Controllo se è stata inviata una richiesta POST per aggiungere una nuova task
-if (isset($_POST['todoItem'])) {
-    // Recupera il valore inviato
-    $newTask = array(
-        'text' => $_POST['todoItem'],
-        'done' => false
-    );
+    // Controllo se è stata inviata una richiesta POST per aggiungere una nuova task
+    if (isset($_POST['todoItem'])) {
+        // Recupera il valore inviato
+        $newTask = array(
+            'text' => $_POST['todoItem'],
+            'done' => false
+        );
 
-    // Aggiunge la nuova task all'array
-    $todoList[] = $newTask;
+        // Aggiunge la nuova task all'array
+        $todoList[] = $newTask;
 
-    // Converte l'array in formato JSON
-    $updatedJsonContent = json_encode($todoList, JSON_PRETTY_PRINT);
+        
 
-    // Salva il contenuto nel file JSON
-    file_put_contents($jsonFilePath, $updatedJsonContent);
-}
+        // Converte l'array in formato JSON
+        $updatedJsonContent = json_encode($todoList, JSON_PRETTY_PRINT);
 
-// Imposta l'intestazione della risposta come JSON
-header('Content-Type: application/json');
+        // Salva il contenuto nel file JSON
+        file_put_contents($jsonFilePath, $updatedJsonContent);
+    }
 
-// Restituisce l'array della lista delle task come JSON
-echo json_encode($todoList);
+    // Imposta l'intestazione della risposta come JSON
+    header('Content-Type: application/json');
+
+    // Restituisce l'array della lista delle task come JSON
+    echo json_encode($todoList);
 ?>
